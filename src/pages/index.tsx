bc10ccf77a323128fc7537c9ca2e4d01e5366b17
Bruno Rocha <obrunorocha@outlook.com>
2021-06-12 15:58:04 -0300

    config prettier with eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,7 @@ type Props = {
   title: string
 }
 
-const foo = 'bla'
-
-export default function Home({ title = "React Avançado" }: Props) {
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -18,9 +16,7 @@ export default function Home({ title = "React Avançado" }: Props) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {title}
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
